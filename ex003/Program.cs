7c51d533d680ea8eaa5918db724f9a427843d94a
Aleksandr <dopeshy.off@gmail.com>
2022-10-31 20:41:01 +0300

    Задание 3

---
+++ ex003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 ==1)
+{ 
+    Console.WriteLine("Число нечётное");
+}
+else  
+{
+ Console.WriteLine("Число чётное");
+}
